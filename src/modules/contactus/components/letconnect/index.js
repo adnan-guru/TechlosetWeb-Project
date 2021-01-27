28ab6514c17bedd444bbed4d2b1c4750d5ca1d23
“Zeeshan <zeeshanraza252627@gmail.com>
2021-01-27 17:07:37 +0500

    slider added.

--- src/modules/contactus/components/letconnect/index.js
+++ src/modules/contactus/components/letconnect/index.js
@@ -3,77 +3,98 @@ import { style } from './style.js';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
-
+import Slider from '../../../../commonComponents/slider/slider.js';
 
 function Contactus() {
-    return (
-<Container>
-        <Grid container >
-            <Grid item md={2} xs={2}></Grid>
-            <Grid item md={8} xs={8}>
-                <Typography style={style.heading} variant='subtitle1'>
-                    Let’s Connect, We are more than happy to help you.
+  return (
+    <Container>
+      <Grid container>
+        {/* <Grid item md={2} xs={2}></Grid> */}
+        <Grid item md={12} xs={12}>
+          <Typography style={style.heading} variant="subtitle1">
+            Let’s Connect, We are more than happy to help you.
           </Typography>
-            </Grid>
-            <Grid item md={3} xs={2}></Grid>
-
-            <Grid container style={style.main}>
-                <Grid item md={3} xs={12}></Grid>
-                <Grid item md={6} xs={12}>
-                    <input style={style.inputs1} type="text" placeholder="NAME" />
-                    <input style={style.inputs2} type="text" placeholder="EMAIL" />
-                    <input style={style.inputs2} type="text" placeholder="PHONE" />
-                    <Grid container>
-                        <Grid item md={6} xs={6}>
-                            <select style={style.menu1}>
-                                <option value="Select a Service">Select a Service</option>
-                                <option value="Mobile Application Development">Mobile Application Development</option>
-                                <option value="Web Development">Web Development</option>
-                                <option value="Artificial Intelligence">Artificial Intelligence</option>
-                                <option value="WordPress">WordPress</option>
-                                <option value="Consulting-Cloud and DevOps">Consulting-Cloud and DevOps</option>
-                                <option value="QA Service">QA Service</option><option value="Other">Other</option>
-                            </select>
-                        </Grid>
-                        <Grid item md={6} xs={6}>
-                            <select style={style.menu2} >
-                                <option value="Select Budget">Select Budget</option>
-                                <option value="<$5K">&lt;$5K</option><option value="<$10K+">&lt;$10K+</option>
-                                <option value="<$25K+">&lt;$25K+</option><option value="<$50K+">&lt;$50K+</option>
-                                <option value="<$100K+">&lt;$100K+</option><option value="Not Sure">Not Sure</option>
-                            </select>
-                        </Grid>
-                        <Grid item md={6} xs={6}>
-                            <select style={style.menu1} >
-                                <option value="How Soon You Want To Start?">How Soon You Want To Start?</option>
-                                <option value="Right Now">Right Now</option>
-                                <option value="In few Weeks">In few Weeks</option>
-                                <option value="In few Months">In few Months</option>
-                                <option value="Not Sure">Not Sure</option>
-                            </select>
-                        </Grid>
-                        <Grid item md={6} xs={6}>
-                            <select style={style.menu2}>
-                                <option value="Requirement">Requirement</option>
-                                <option value="Hire Dedicated Team">Hire Dedicated Team</option>
-                                <option value="New Project">New Project</option>
-                                <option value="Existing Project">Existing Project</option>
-                                <option value="Support-Maintenance">Support-Maintenance</option>
-                            </select>
-                        </Grid>
-                        <Grid col md={12} xs={12}>
-                            <textarea style={style.option} cols="40" rows="10" placeholder="Brief About The Project" />
-                        </Grid>
-                        <Grid col md={12} xs={12}>
-                        </Grid>
-                        <input style={style.contact} type="submit" value="LET'S TALK" />
-                    </Grid>
-                </Grid>
-                <Grid item md={3} xs={12}></Grid>
-            </Grid>
         </Grid>
-        </Container>
 
-    )
+        <Grid container item style={style.main}>
+          <Grid item md={5} xs={12} style={style.slider}>
+            <Slider />
+          </Grid>
+          <Grid item md={1} >
+          </Grid>
+          <Grid container item md={6} xs={12} sm={6}>
+            <input style={style.inputs1} type="text" placeholder="NAME" />
+            <input style={style.inputs2} type="text" placeholder="EMAIL" />
+            <input style={style.inputs2} type="text" placeholder="PHONE" />
+            <Grid container>
+              <Grid item md={6} xs={6} sm={6}>
+                <select style={style.menu1}>
+                  <option value="Select a Service">Select a Service</option>
+                  <option value="Mobile Application Development">
+                    Mobile Application Development
+                  </option>
+                  <option value="Web Development">Web Development</option>
+                  <option value="Artificial Intelligence">
+                    Artificial Intelligence
+                  </option>
+                  <option value="WordPress">WordPress</option>
+                  <option value="Consulting-Cloud and DevOps">
+                    Consulting-Cloud and DevOps
+                  </option>
+                  <option value="QA Service">QA Service</option>
+                  <option value="Other">Other</option>
+                </select>
+              </Grid>
+              <Grid item md={6} xs={6}>
+                <select style={style.menu2}>
+                  <option value="Select Budget">Select Budget</option>
+                  <option value="<$5K">&lt;$5K</option>
+                  <option value="<$10K+">&lt;$10K+</option>
+                  <option value="<$25K+">&lt;$25K+</option>
+                  <option value="<$50K+">&lt;$50K+</option>
+                  <option value="<$100K+">&lt;$100K+</option>
+                  <option value="Not Sure">Not Sure</option>
+                </select>
+              </Grid>
+              <Grid item md={6} xs={6}>
+                <select style={style.menu1}>
+                  <option value="How Soon You Want To Start?">
+                    How Soon You Want To Start?
+                  </option>
+                  <option value="Right Now">Right Now</option>
+                  <option value="In few Weeks">In few Weeks</option>
+                  <option value="In few Months">In few Months</option>
+                  <option value="Not Sure">Not Sure</option>
+                </select>
+              </Grid>
+              <Grid item md={6} xs={6}>
+                <select style={style.menu2}>
+                  <option value="Requirement">Requirement</option>
+                  <option value="Hire Dedicated Team">
+                    Hire Dedicated Team
+                  </option>
+                  <option value="New Project">New Project</option>
+                  <option value="Existing Project">Existing Project</option>
+                  <option value="Support-Maintenance">
+                    Support-Maintenance
+                  </option>
+                </select>
+              </Grid>
+              <Grid col md={12} xs={12}>
+                <textarea
+                  style={style.option}
+                  cols="40"
+                  rows="10"
+                  placeholder="Brief About The Project"
+                />
+              </Grid>
+              <Grid col md={12} xs={12}></Grid>
+              <input style={style.contact} type="submit" value="LET'S TALK" />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 export default Contactus;
--- src/modules/contactus/components/letconnect/style.js
+++ src/modules/contactus/components/letconnect/style.js
@@ -1,67 +1,65 @@
-import { colors } from '../../../../constants/color'
+import { colors } from '../../../../constants/color';
 export const style = {
-    heading: {
-        marginTop: '50px',
-        textAlign: 'center',
-        fontSize: '22px',
-        letterSpacing: '2px',
-        fontWeight: '300',
-        color: colors.primaryColor
-    },
+  heading: {
+    marginTop: '50px',
+    textAlign: 'center',
+    fontSize: '22px',
+    letterSpacing: '2px',
+    fontWeight: '300',
+    color: colors.primaryColor,
+  },
 
-    main: {
-        paddingTop: '50px',
-    },
-    inputs1: {
-        height: '50px',
-        width: '100%',
-        outline: 'none',
-
-
-    },
-    inputs2: {
-        marginTop: '20px',
-        height: '50px',
-        width: '100%',
-        outline: 'none',
-    },
-    menu1: {
-        marginTop: '20px',
-        height: '50px',
-        width: '100%',
-        outline: 'none',
-        color: colors.primaryColor
-
-    },
-    menu2: {
-        marginTop: '20px',
-        marginLeft:'7px',
-        height: '50px',
-        width: '100%',
-        outline: 'none',
-        color: colors.primaryColor
-
-    },
-    option: {
-        fontSize: '15px',
-        marginTop: '20px',
-        height: '150px',
-        width: '100%',
-        outline: 'none',
-        color: colors.primaryColor
-    },
-    contact: {
-        marginTop: '20px',
-        backgroundColor: colors.secondryColor,
-        color: '#fff',
-        letterSpacing: '2px',
-        fontWeight: '400',
-        borderRadius: '25px',
-        border: 'none',
-        padding: '13px 29px 13px 29px',
-        fontSize: '14px',
-        lineHeight: '17px',
-        outline: 'none',
-    },
-   
-}
+  main: {
+    paddingTop: '50px',
+  },
+  inputs1: {
+    height: '50px',
+    width: '100%',
+    outline: 'none',
+  },
+  inputs2: {
+    marginTop: '20px',
+    height: '50px',
+    width: '100%',
+    outline: 'none',
+  },
+  menu1: {
+    marginTop: '20px',
+    height: '50px',
+    width: '100%',
+    outline: 'none',
+    color: colors.primaryColor,
+  },
+  menu2: {
+    marginTop: '20px',
+    marginLeft: '7px',
+    height: '50px',
+    width: '100%',
+    outline: 'none',
+    color: colors.primaryColor,
+  },
+  option: {
+    fontSize: '15px',
+    marginTop: '20px',
+    height: '150px',
+    width: '100%',
+    outline: 'none',
+    color: colors.primaryColor,
+  },
+  contact: {
+    marginTop: '20px',
+    backgroundColor: colors.secondryColor,
+    color: '#fff',
+    letterSpacing: '2px',
+    fontWeight: '400',
+    borderRadius: '25px',
+    border: 'none',
+    padding: '13px 29px 13px 29px',
+    fontSize: '14px',
+    lineHeight: '17px',
+    outline: 'none',
+  },
+  slider: {
+    marginBottom: '50px',
+  },
+};
